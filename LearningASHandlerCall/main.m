1a495465d0655de9c34fe33c271b35e864e81f80
lemanouche0221 <AnkhoD@MacBook-Pro-de-AnkhoD.local>
2014-04-22 14:32:06 +0200

    Initial Commit

---
+++ LearningASHandlerCall/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  LearningASHandlerCall
+//
+//  Created by AnkhoD on 22/04/2014.
+//  Copyright (c) 2014 AnkhoD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
